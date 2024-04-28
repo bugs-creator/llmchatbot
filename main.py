d981055b04504ad6a394a6e65e3e6e235da65e0c
ZhenqiHe <72725463+zhenqi-he@users.noreply.github.com>
2024-04-28 22:57:59 +0800

    add comments

--- main.py
+++ main.py
@@ -30,17 +30,21 @@ base_model = args.model
 # New instruction dataset
 guanaco_dataset = args.dataset
 
-# Fine-tuned model
+# output directory
 new_model = args.output_dir
 if args.dataset_path is None:
+    # if do not give a dataset_path, it will load the default one from hugging face
     dataset = load_dataset(guanaco_dataset, split="train")
 else:
-    # dataset = llama_dataset.LlamaDataset(file=args.dataset_path)
+    # if given a dataset_path, it will use our combined self-collected data and public data
     dataset = load_dataset("csv", data_files=args.dataset_path)
+    # data preprocessing to the required format
     dataset = dataset.map(llama_dataset.transform_conversation)['train']
 
+# using float 16
 compute_dtype = getattr(torch, "float16")
 
+# load config document
 quant_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
@@ -48,6 +52,7 @@ quant_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=False,
 )
 
+# load model from pretrained weights
 model = AutoModelForCausalLM.from_pretrained(
     base_model,
     quantization_config=quant_config,
@@ -56,10 +61,12 @@ model = AutoModelForCausalLM.from_pretrained(
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
+# load tokenizer
 tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
+# lora config setting
 peft_params = LoraConfig(
     lora_alpha=16,
     lora_dropout=0.1,
@@ -68,6 +75,7 @@ peft_params = LoraConfig(
     task_type="CAUSAL_LM",
 )
 
+# set related training paramteres
 training_params = TrainingArguments(
     output_dir="/root/autodl-tmp/llm_training_outputs_update_wonhs",
     num_train_epochs=1,
@@ -88,6 +96,7 @@ training_params = TrainingArguments(
     report_to="tensorboard"
 )
 
+# initialize trainer
 trainer = SFTTrainer(
     model=model,
     train_dataset=dataset,
@@ -99,11 +108,11 @@ trainer = SFTTrainer(
     packing=False,
 )
 
+# start to train
 trainer.train()
 
+# save fine-tuned model
 trainer.model.save_pretrained(new_model)
 trainer.tokenizer.save_pretrained(new_model)
 
-from tensorboard import notebook
-# log_dir = "/root/tf-logs/"
-# notebook.start("--logdir {} --port 4000".format(log_dir))
+
